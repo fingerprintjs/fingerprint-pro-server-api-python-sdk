f09e0eacd7d0afe449d81d689cfe2ad6680f3122
Ilya Taratukhin <darkilfa@gmail.com>
2022-07-29 18:47:05 +0300

    feat: use config.json to store generation options

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@
 from setuptools import setup, find_packages  # noqa: H301
 
 NAME = "fingerprint-pro-server-api-sdk"
-VERSION = "1.0.0"
+VERSION = "0.0.1"
 # To install the library, run the following
 #
 # python setup.py install
