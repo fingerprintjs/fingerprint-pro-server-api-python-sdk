e6b6208640820ea38a913e60b04634185b0a4ffc
Ilya Taratukhin <darkilfa@gmail.com>
2022-08-03 22:37:53 +0300

    chore: update mocks for tests during schema generation

--- generate.sh
+++ generate.sh
@@ -1,3 +1,13 @@
-#!/bin/sh
+#!/bin/bash
 
 swagger-codegen generate -t ./template -l python -i https://fingerprintjs.github.io/fingerprint-pro-server-api-openapi/schemes/fingerprint-server-api.yaml -o ./ -c config.json
+
+examplesList=(
+  'visits_limit_1.json'
+  'visits_limit_500.json'
+  'webhook.json'
+)
+
+for example in ${examplesList[*]}; do
+  curl -o ./test/mocks/"$example" https://fingerprintjs.github.io/fingerprint-pro-server-api-openapi/examples/"$example"
+done
