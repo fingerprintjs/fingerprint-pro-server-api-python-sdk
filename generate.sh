492bc77bca510e7e3e44918af55c8f6b8d48a775
Ilya Taratukhin <darkilfa@gmail.com>
2022-08-12 17:39:24 +0300

    chore: use local swagger-codegen-cli.jar to generate schema

--- generate.sh
+++ generate.sh
@@ -10,4 +10,6 @@ while getopts "v:" arg; do
   esac
 done
 
-swagger-codegen generate -t ./template -l python -i ./res/fingerprint-server-api.yaml -o ./ -c config.json -DpackageVersion=$VERSION
+# jar was downloaded from here https://repo1.maven.org/maven2/io/swagger/codegen/v3/swagger-codegen-cli/3.0.34/
+
+java -jar ./bin/swagger-codegen-cli.jar generate -t ./template -l python -i ./res/fingerprint-server-api.yaml -o ./ -c config.json -DpackageVersion=$VERSION
