a0c9a9f01f4e273da967e6ed78c7a8b93ac571be
Ilya Taratukhin <darkilfa@gmail.com>
2022-08-12 13:41:37 +0300

    chore: support -v argument in generate function

--- generate.sh
+++ generate.sh
@@ -2,4 +2,12 @@
 
 VERSION='0.0.1'
 
+while getopts "v:" arg; do
+  case $arg in
+    v)
+      VERSION=$OPTARG
+      ;;
+  esac
+done
+
 swagger-codegen generate -t ./template -l python -i ./res/fingerprint-server-api.yaml -o ./ -c config.json -DpackageVersion=$VERSION
