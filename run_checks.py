f1f36295cff558e30a1489a4e5fb027077fb70dd
Ilya Taratukhin <ilya.taratukhin@fingerprint.com>
2025-03-25 13:53:48 +0100

    test: add check for functional tests to check old events

--- run_checks.py
+++ run_checks.py
@@ -1,4 +1,5 @@
 import os
+from datetime import datetime, timedelta
 
 import fingerprint_pro_server_api_sdk
 from fingerprint_pro_server_api_sdk.rest import ApiException
@@ -13,15 +14,19 @@ configuration = fingerprint_pro_server_api_sdk.Configuration(
 # create an instance of the API class
 api_instance = fingerprint_pro_server_api_sdk.FingerprintApi(configuration)
 
+end = int(datetime.now().timestamp() * 1000)
+start = int((datetime.now() - timedelta(days=90)).timestamp() * 1000)
+
+
 # FingerprintApi->search_events usage example
 try:
-    search_events_response = api_instance.search_events(2)
+    search_events_response = api_instance.search_events(2, start=start, end=end)
     first_event = search_events_response.events[0]
     first_event_identification_data = first_event.products.identification.data
     visitor_id = first_event_identification_data.visitor_id
     request_id = first_event_identification_data.request_id
     print("\n\n\nSearch events response: \n", search_events_response)
-    search_events_response_second_page = api_instance.search_events(2, pagination_key=search_events_response.pagination_key)
+    search_events_response_second_page = api_instance.search_events(2, start=start, end=end, pagination_key=search_events_response.pagination_key)
 
     if len(search_events_response_second_page.events) == 0:
         print("Second page of FingerprintApi->search_events: is empty")
@@ -60,6 +65,19 @@ try:
 except ApiException as e:
     print("Exception when calling Async example: %s\n" % e)
     exit(1)
+
+# Check that old events are still match expected format
+try:
+    search_events_response_old = api_instance.search_events(1, start=start, end=end, reverse=True)
+    old_event_identification_data = search_events_response_old.events[0].products.identification.data
+    visitor_id_old = old_event_identification_data.visitor_id
+    request_id_old = old_event_identification_data.request_id
+
+    api_instance.get_visits(visitor_id_old, limit=2)
+    api_instance.get_event(request_id_old)
+    print("\n\n\nOld events are good\n")
+except ApiException as e:
+    print("Exception when trying to read old data: %s\n" % e)
 
 print("Checks passed!")
 
