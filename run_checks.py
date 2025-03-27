093e44511d36a4ee880a65793cbac5830f56e587
Ilya Taratukhin <ilya.taratukhin@fingerprint.com>
2025-03-27 13:51:57 +0100

    chore: apply review findings

--- run_checks.py
+++ run_checks.py
@@ -21,6 +21,9 @@ start = int((datetime.now() - timedelta(days=90)).timestamp() * 1000)
 # FingerprintApi->search_events usage example
 try:
     search_events_response = api_instance.search_events(2, start=start, end=end)
+    if len(search_events_response.events) == 0:
+        print("FingerprintApi.search_events: is empty")
+        exit(1)
     first_event = search_events_response.events[0]
     first_event_identification_data = first_event.products.identification.data
     visitor_id = first_event_identification_data.visitor_id
@@ -29,11 +32,11 @@ try:
     search_events_response_second_page = api_instance.search_events(2, start=start, end=end, pagination_key=search_events_response.pagination_key)
 
     if len(search_events_response_second_page.events) == 0:
-        print("Second page of FingerprintApi->search_events: is empty")
+        print("Second page of FingerprintApi.search_events: is empty")
         exit(1)
 
 except ApiException as e:
-    print("Exception when calling FingerprintApi->search_events: %s\n" % e)
+    print("Exception when calling FingerprintApi.search_events: %s\n" % e)
     exit(1)
 
 # Use existing visitor_id from FingerprintApi->search_events response to check FingerprintApi->get_visits method
@@ -42,7 +45,7 @@ try:
     print("\n\n\nVisits response: \n", visits_response)
 
 except ApiException as e:
-    print("Exception when calling FingerprintApi->get_visits: %s\n" % e)
+    print("Exception when calling FingerprintApi.get_visits: %s\n" % e)
     exit(1)
 
 # Use existing request_id from FingerprintApi->search_events response to check FingerprintApi->get_event method
@@ -51,7 +54,7 @@ try:
     print("\n\n\nEvent response: \n", events_response.products)
 
 except ApiException as e:
-    print("Exception when calling FingerprintApi->get_event: %s\n" % e)
+    print("Exception when calling FingerprintApi.get_event: %s\n" % e)
     exit(1)
 
 # Async methods examples
@@ -69,9 +72,16 @@ except ApiException as e:
 # Check that old events are still match expected format
 try:
     search_events_response_old = api_instance.search_events(1, start=start, end=end, reverse=True)
+    if len(search_events_response_old.events) == 0:
+        print("FingerprintApi.search_events: is empty for old events\n")
+        exit(1)
     old_event_identification_data = search_events_response_old.events[0].products.identification.data
     visitor_id_old = old_event_identification_data.visitor_id
     request_id_old = old_event_identification_data.request_id
+
+    if visitor_id_old == visitor_id or request_id_old == request_id:
+        print("Old events are identical to new\n")
+        exit(1)
 
     api_instance.get_visits(visitor_id_old, limit=2)
     api_instance.get_event(request_id_old)
